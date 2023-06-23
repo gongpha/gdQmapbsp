651abc19b46eb153432aa93763de7686bfcacd51
Victor Stan <victor.stan@gmail.com>
2023-06-23 11:58:51 -0400

    DOORS: trigers generated for platforms without targetnames

--- quake1_example/class/func_plat.gd
+++ quake1_example/class/func_plat.gd
@@ -3,21 +3,20 @@ class_name QmapbspQuakeFunctionPlat
 # FIXME: trigger volume needs to extend through the entire travel path of the plat
 
 func _can_create_trigger() -> bool : return true
-func _get_trigger_padding() -> Vector3 : return Vector3(0, 0.25, 0)
+func _get_trigger_padding() -> Vector3 : return Vector3(-0.75, -0.5, -0.75)
 
 func _gen_aabb() :
 	var height : int = props.get('height', '0').to_int()
-	if height == 0 :
-		super()
-	else :
-		aabb.size.y = height
+	if height == 0 : super()
+	else : aabb.size.y = height
 	aabb.size.y *= -1
+	
+func _set_primary_trigger_position(col : CollisionShape3D, aabb: AABB) :
+	super(col, aabb)
+	col.position.y -= aabb.size.y
 	
 func _def_lip() -> String : return '-8' # >O_O<
 func _no_linking() -> bool : return false
-func _player_touch(p : QmapbspQuakePlayer, pos : Vector3, nor : Vector3) :
-	if open and nor.y > 0.9 :
-		_trigger(p)
 		
 func _motion_f(destroy_tween : bool = false) :
 	super(true)
