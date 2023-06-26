14a1b1ae6b88ebd0782682765705b240359eff6c
Victor Stan <victor.stan@gmail.com>
2023-06-26 09:06:51 -0400

    Update quake1_example/fluid_lava.gd
    Co-authored-by: Kongfa Waroros <gongpha@hotmail.com>

--- quake1_example/fluid_lava.gd
+++ quake1_example/fluid_lava.gd
@@ -2,4 +2,4 @@ extends QmapbspQuakeFluidVolume
 class_name QmapbspQuakeLavaVolume
 
 func _damage() -> int : return 10
-func _liquid_type() -> String : return 'lava'
+func _liquid_type() -> StringName : return &'lava'
