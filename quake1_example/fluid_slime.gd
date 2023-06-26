9462980584cf781253edce76b17a0895232c32d3
Victor Stan <victor.stan@gmail.com>
2023-06-26 09:05:59 -0400

    Update quake1_example/fluid_slime.gd
    Co-authored-by: Kongfa Waroros <gongpha@hotmail.com>

--- quake1_example/fluid_slime.gd
+++ quake1_example/fluid_slime.gd
@@ -2,5 +2,5 @@ extends "res://quake1_example/fluid.gd"
 class_name QmapbspQuakeSlimeVolume
 
 func _damage() -> int : return 2
-func _liquid_type() -> String : return 'slime'
-func _decay_time() -> int : return 5 # in seconds
+func _liquid_type() -> StringName : return &'slime'
+func _decay_time() -> float : return 5 # in seconds
