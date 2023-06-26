71612fbe06c9630cc1930392ea2f3d6598cda50e
Victor Stan <victor.stan@gmail.com>
2023-06-26 11:58:52 -0400

    missing conflict changes

--- quake1_example/fluid_lava.gd
+++ quake1_example/fluid_lava.gd
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
-extends "res://quake1_example/fluid.gd"
-class_name QmapbspQuakeLavaVolume
-
-func _damage() -> int : return 10
-func _liquid_type() -> String : return 'lava'
-=======
 extends QmapbspQuakeFluidVolume
 class_name QmapbspQuakeLavaVolume
 
 func _damage() -> int : return 10
 func _liquid_type() -> StringName : return &'lava'
->>>>>>> liquid-volumes
