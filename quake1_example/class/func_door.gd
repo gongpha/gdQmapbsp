8b96a34241b04613ce74595c0c4732a5554a52c9
Victor Stan <victor.stan@gmail.com>
2023-07-15 13:20:15 -0400

    door sounds, animations and bugfixes

--- quake1_example/class/func_door.gd
+++ quake1_example/class/func_door.gd
@@ -15,16 +15,54 @@ const DONT_LINK : int = 4;
 const GOLD_KEY : int = 8;
 const SILVER_KEY : int = 16;
 const TOGGLE : int = 32;
+# Properties defaults:
+const LIP : int = 8
+const WAIT : int = 3
+const SPEED : int = 100
+const DMG : int = 2
 
-const GOLD_KEY_MESSAGE = 'You require the Gold Key!'
-const SILVER_KEY_MESSAGE = 'You require the Silver Key!'
+const TRIGGER_PADDING : Vector3 = Vector3(1.8, 0.25, 1.8)
+
+# 0: "Silent"
+# 1: "Stone"
+# 2: "Machine"
+# 3: "Stone Chain"
+# 4: "Screechy Metal"
+const audio_paths := [
+	['misc/null.wav', 'misc/null.wav'],
+	['doors/drclos4.wav', 'doors/doormv1.wav'],
+	['doors/hydro1.wav', 'doors/hydro2.wav'],
+	['doors/stndr1.wav', 'doors/stndr2.wav'],
+	['doors/ddoor1.wav', 'doors/ddoor2.wav']
+]
+
+const keys_audio_paths := [
+	['doors/medtry.wav', 'doors/meduse.wav'],
+	['doors/runetry.wav', 'doors/runeuse.wav'],
+	['doors/basetry.wav', 'doors/baseuse.wav'],
+]
+
+# 0 : "Medieval"
+# 1 : "Metal (runic)"
+# 2 : "Base"
+const gold_keys_message = [
+	"You need the gold key",
+	"You need the gold runekey",
+	"You need the gold keycard"
+]
+const silver_key_message = [
+	"You need the silver key",
+	"You need the silver runekey",
+	"You need the silver keycard"
+]
 
 var tween : Tween
 var add : Vector3
 var dura : float
 var wait : int
 var calc_ : bool = false
-var streams : Array
+var noise_door : Array
+var noise_key : Array
 var viewer : QmapbspQuakeViewer
 var open : bool = false
 var open_pos : Vector3
@@ -38,34 +76,13 @@ var trigger : QmapbspQuakeTrigger
 
 signal emit_message_once(m : String)
 
-# according to the QC file
-const audio_paths := [
-	['', ''],
-	['doors/drclos4.wav', 'doors/doormv1.wav'],
-	['doors/hydro1.wav', 'doors/hydro2.wav'],
-	['doors/stndr1.wav', 'doors/stndr2.wav'],
-	['doors/ddoor1.wav', 'doors/ddoor2.wav'],
-	['', ''],
-]
-
-var func_door := [
-	['doors/medtry.wav', 'doors/meduse.wav'],
-	['doors/runetry.wav', 'doors/runeuse.wav'],
-	['doors/basetry.wav', 'doors/baseuse.wav'],
-]
-
-func _def_lip() -> String : return '8'
-func _def_wait() -> String : return '3'
-func _def_speed() -> String : return '100'
-
-func _get_trigger_padding() -> Vector3 : return Vector3(1.8, 0.25, 1.8)
-func _get_sound_index_loop() -> int : return 0
-func _get_sound_index_motion_end() -> int : return 1
 
 func _map_ready() :
 	init_pos = position
 	add_to_group(&'doors')
+	_gen_aabb()
 	_calc_add()
+	_get_sounds()
 	_calc_anim_pos()
 	
 	
@@ -76,39 +93,25 @@ func _entities_ready() :
 func _doors_ready() :
 	if is_in_group(&'primary_doors') and not props.has(&'targetname') : 
 		_create_primary_trigger()
-	if _starts_open() : 
-		_open_direct()
-
-
-func _starts_open() -> bool :
-	return _flag(START_OPEN)
+	if _flag(START_OPEN) : _open_direct()
 
 
 func _add_link(n : QmapbspQuakeFunctionDoor) :
 	if n == self : return
 	if links.has(n) : return
+	
 	links.append(n)
-
-
-func _no_linking() -> bool :
-	return _flag(DONT_LINK)
-
-
-func _get_angle() -> int :
-	return props.get('angle', 0)
 
 
 func _calc_add() :
 	if !calc_ :
 		calc_ = true
 		
-		_gen_aabb()
-		
-		if not _no_linking() :
+		if not _flag(DONT_LINK) :
 			# find doors to try and link
 			for n in get_tree().get_nodes_in_group(&'doors') :
 				n._calc_add()
-				if n._no_linking() : continue
+				if n._flag(DONT_LINK) : continue
 				# check if doors are touching
 				if (
 					aabb.size.x >= 0 and aabb.size.y >= 0 and
@@ -119,10 +122,10 @@ func _calc_add() :
 						n._add_link(self)
 		
 		viewer = get_meta(&'viewer')
-		var angle : int = _get_angle()
+		var angle : int = _prop(&'angle', 0)
 		var s : float = get_meta(&'scale', 32.0)
-		wait = props.get(&'wait', _def_wait()).to_int()
-		var lip : float = props.get(&'lip', _def_lip()).to_int() / s
+		wait = _prop(&'wait', WAIT)
+		var lip : float = _prop(&'lip', LIP) / s
 		if angle == -1 :
 			add = Vector3(0.0, aabb.size.y - lip, 0.0)
 		elif angle == -2 :
@@ -132,14 +135,11 @@ func _calc_add() :
 			var dir := Vector3(aabb.size.x, 0.0, aabb.size.z)
 			var lip_v := Vector3(lip, 0.0, lip)
 			add = Vector3.BACK.rotated(Vector3.UP, rot) * (-dir + lip_v)
-		dura = add.length() / (props.get(&'speed', _def_speed()).to_int() / s)
-		
-		var sounds : int = clampi(props.get(&'sounds', '0').to_int(), 0, 5)
-		_get_sounds(sounds)
+		dura = add.length() / (_prop(&'speed', SPEED) / s)
 
 
 func _calc_anim_pos() :
-	if _starts_open() :
+	if _flag(START_OPEN) :
 		open_pos = init_pos + add
 		close_pos = init_pos
 	else :
@@ -152,7 +152,6 @@ func _set_primary() :
 	if _requires_key() : return
 	
 	var make_primary : bool = true
-	var l_targetname : String
 	for l in links :
 		if l.has_meta(&'primary') : make_primary = false
 		if l.props.has(&'targetname') : make_primary = false
@@ -180,7 +179,7 @@ func _create_primary_trigger() :
 	var col = CollisionShape3D.new()
 	col.name = &'col_shape'
 	col.shape = BoxShape3D.new()
-	var t_pad = _get_trigger_padding()
+	var t_pad = TRIGGER_PADDING
 	var t_pos : Vector3 = aabb.position - t_pad
 	var t_end : Vector3 = aabb.end + t_pad
 	col.shape.size = (t_end - t_pos).abs()
@@ -205,7 +204,7 @@ func _update_trigger_shape(
 ) :
 	# calc new size
 	var m_aabb : AABB = aabb.merge(new_door.aabb)
-	var t_pad = _get_trigger_padding()
+	var t_pad = TRIGGER_PADDING
 	var t_pos : Vector3 = m_aabb.position - t_pad
 	var t_end : Vector3 = m_aabb.end + t_pad
 	# update shape size and position
@@ -215,97 +214,87 @@ func _update_trigger_shape(
 		col.set_position(m_aabb.get_center())
 		
 		
-func _get_sounds(sounds : int) :
-	if sounds == 5 :
-		streams = func_door[
-			viewer.worldspawn.props.get('worldtype', ['', '']) % 3
-		]
+func _is_blocked() -> bool :
+	# check if player has not exited the trigger area
+	if trigger and trigger.blocked : return true
 	else :
-		streams = audio_paths[sounds]
+		for l in links :
+			if l.trigger and l.trigger.blocked : return true
+	return false
 		
 		
 func _trigger(b : Node3D) :
-	if _requires_gold_key() : emit_message_once.emit(GOLD_KEY_MESSAGE)
-	elif _requires_silver_key() : emit_message_once.emit(SILVER_KEY_MESSAGE)
-	if _requires_key() : return # TODO: fetch key from player
+	# TODO: check player for key
+	if _requires_key() :
+		# doors that require keys, stay open
+		if open : return
+		else :
+			_play_snd(&'key_try')
+			_show_key_message()
+		return
 	if tween : return
-	_move()
-	for l in links :
-		l._trigger(b)
-		
-		
-func _make_player() :
-	if !player :
-		player = AudioStreamPlayer3D.new()
-		player.finished.connect(_audf)
-		add_child(player)
-		
-		
-func _play_snd(idx : int) :
-	_make_player()
-	var s : String = streams[idx]
-	if s.is_empty() : return
-	player.stream = viewer.hub.load_audio(s)
-	player.play()
 	
-	
-func _audf() :
-	if player_end :
-		player.queue_free()
-		player = null
-	else :
-		_make_player()
-		player.play()
-
-
-func _move_pre(tween : Tween) -> Vector3 : 
-	if open : return close_pos
-	else : return open_pos
-	
-
-# TODO: check if something is blocking the door before closing
-# TODO: apply damage to player if blocking and trying to close door
-func _move() :
+	_move_toggle()
+	for l in links : l._trigger(b)
+		
+		
+func _trigger_exit(b : Node3D) :
+	if _requires_key() : return
 	if tween : return
-
+	
+	if not _flag(TOGGLE) :
+		_move_close()
+		for l in links : l._trigger_exit(b)
+	
+	
+func _move_open() :
+	if tween : return
+	if open : return
+	
 	tween = create_tween()
-	var target_pos : Vector3 = _move_pre(tween) 
-	tween.tween_property(self, ^'position',
-		target_pos, dura
-	)
-	if wait > 0 : # add delay
-		if not open and not _starts_open() :
-			tween.tween_interval(wait)
-		elif open and _starts_open() :
-			tween.tween_interval(wait)
+	tween.tween_callback(_play_snd.bind(&'door_loop'))
+	tween.tween_property(self, ^'position', open_pos, dura)
+	tween.tween_callback(_play_snd.bind(&'door_impulse'))
+	if not _flag(START_OPEN) : tween.tween_interval(wait) # wait after animation
 	tween.finished.connect(_move_end)
 	
-	open = !open
+	open = true
+
+
+func _move_close() :
+	if tween : return
+	if not open : return
+	if _is_blocked() : return
+
+	tween = create_tween()
+	tween.tween_callback(_play_snd.bind(&'door_loop'))
+	tween.tween_property(self, ^'position', close_pos, dura)
+	tween.tween_callback(_play_snd.bind(&'door_impulse'))
+	if _flag(START_OPEN) : tween.tween_interval(wait) # wait after animation
+	tween.finished.connect(_move_end)
 	
-	_play_snd(_get_sound_index_loop())
-	player_end = true
+	open = false
+
+
+func _move_toggle() :
+	if open : _move_close()
+	else : _move_open()
 
 
 func _move_end() :
 	tween.kill()
 	tween = null
 	
-	# sound
-	player_end = true
-	_play_snd(_get_sound_index_motion_end())
+	if wait == -1 : return # stay open
+	if _is_blocked() : return
+	if _flag(TOGGLE) : return
 	
-	if wait == -1 : return # permanently open
-	
-	# close
-	# TODO: check if player is still inside the trigger
-	if _starts_open() and not open :
-		_move()
-	elif not _starts_open() and open :
-		_move()
+	if _flag(START_OPEN) : 
+		if not open : _move_open()
+	elif open : _move_close()
 
 
 func _open_direct() :
-#	player_end = false
 	position += add
 	init_pos = position
 	open = true
@@ -314,13 +303,15 @@ func _open_direct() :
 func _player_touch(p : QmapbspQuakePlayer, pos : Vector3, nor : Vector3) :
 	var can_trigger : bool = true
 	if props.has(&'targetname') : can_trigger = false
-	if props.has('message') : 
-		emit_message_once.emit(props['message'])
+	if props.has(&'message') : 
+		if not _prop(&'message', '').is_empty(): 
+			emit_message_once.emit(_prop(&'message', ''))
 	
 	for l in links :
 		if l.props.has(&'targetname') : can_trigger = false
-		if l.props.has('message') :
-			l.emit_message_once.emit(l.props['message'])
+		if l.props.has(&'message') :
+			if not _prop(&'message', '').is_empty(): 
+				l.emit_message_once.emit(l._prop(&'message', ''))
 	
 	if can_trigger : _trigger(p)
 
@@ -335,3 +326,37 @@ func _requires_silver_key() -> bool :
 
 func _requires_gold_key() -> bool :
 	return _flag(GOLD_KEY)
+
+
+func _show_key_message() -> void :
+	var world_idx : int = viewer.worldspawn.props.get(&'worldtype', 0)
+	if _requires_gold_key() : 
+		emit_message_once.emit(gold_keys_message[world_idx])
+	elif _requires_silver_key() : 
+		emit_message_once.emit(silver_key_message[world_idx])
+
+
+func _get_sounds() :
+	var world_idx : int = viewer.worldspawn.props.get(&'worldtype', 0)
+	var noise_idx : int = _prop(&'sounds', 0)
+
+	noise_key = keys_audio_paths[world_idx]
+	noise_door = audio_paths[noise_idx]
+
+
+func _make_player() :
+	player = AudioStreamPlayer3D.new()
+	add_child(player)
+
+
+func _play_snd(sname : StringName) :
+	if !player : _make_player()
+	player.stop()
+	var audio_path : String
+	match sname :
+		&'door_impulse' : audio_path = noise_door[1]
+		&'door_loop' : audio_path = noise_door[0]
+		&'key_try' : audio_path = noise_key[0]
+		&'key_use' : audio_path = noise_key[1]
+	player.stream = viewer.hub.load_audio(audio_path)
+	player.play()
