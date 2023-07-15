c9de65be2d1dbb00fa1be906915596f1a40bb082
Victor Stan <victor.stan@gmail.com>
2023-07-15 13:20:56 -0400

    train sound updates

--- quake1_example/class/func_train.gd
+++ quake1_example/class/func_train.gd
@@ -8,28 +8,11 @@ var corner : Vector3
 var player : AudioStreamPlayer3D
 var player_end := false
 
-func _make_player() :
-	if !player :
-		player = AudioStreamPlayer3D.new()
-		player.finished.connect(_audf)
-		add_child(player)
-		
-func _audf() :
-	if player_end :
-		player.queue_free()
-		player = null
-	else :
-		_make_player()
-		player.play()
-		
-func _play_snd(path : String) :
-	_make_player()
-	player.stream = get_meta(&'viewer').hub.load_audio(path)
-	player.play()
 
 func _map_ready() :
 	_after_map_ready.call_deferred()
-	
+
+
 func _after_map_ready() :
 	var path : Node = get_tree().get_first_node_in_group(
 		'T_' + props.get('target')
@@ -43,10 +26,12 @@ func _after_map_ready() :
 			corner.x = -corner.x
 			position = curve.get_point_position(0) + corner
 
+
 func _trigger(b : Node3D) :
 	if curve == null : return
 	_start(curve)
-		
+
+
 func _start(c : Curve3D) :
 	if tween : return
 	var s : float = get_meta(&'scale', 32.0)
@@ -68,6 +53,29 @@ func _start(c : Curve3D) :
 	player_end = false
 	_play_snd('plats/train1.wav')
 
+
 func _f() :
 	player_end = true
 	_play_snd('plats/train2.wav')
+
+
+func _make_player() :
+	if !player :
+		player = AudioStreamPlayer3D.new()
+		player.finished.connect(_audf)
+		add_child(player)
+
+
+func _audf() :
+	if player_end :
+		player.queue_free()
+		player = null
+	else :
+		_make_player()
+		player.play()
+
+
+func _play_snd(path : String) :
+	_make_player()
+	player.stream = get_meta(&'viewer').hub.load_audio(path)
+	player.play()
