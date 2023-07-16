4988dea9a25f898b48043c7a6df1e391bfff81a9
Victor Stan <victor.stan@gmail.com>
2023-07-16 11:23:55 -0400

    update to trigger counter

--- quake1_example/class/trigger_counter.gd
+++ quake1_example/class/trigger_counter.gd
@@ -4,6 +4,7 @@ class_name QmapbspQuakeTriggerCounter
 # Spawnflags:
 # 1: "No Message" : 0
 const NO_MESSAGE : int = 1
+const COUNT : int = 2
 
 var counter : int = 0
 var counter_max : int = 0
@@ -13,10 +14,17 @@ func _bo_ex(b : Node3D) : return
 
 
 func _map_ready() :
-	counter_max = props.get('count', '0').to_int()
+	counter_max = _prop(&'count', COUNT)
 
 
 func _trigger(b : Node3D) :
 	counter += 1
+	
+	if not _flag(NO_MESSAGE) :
+		if (counter_max - counter) >= 4 : _message("There are more to go...")
+		elif (counter_max - counter) == 3 : _message("Only 3 more to go...")
+		elif (counter_max - counter) == 2 : _message("Only 2 more to go...")
+		elif (counter_max - counter) == 1 : _message("Only 1 more to go...")
+
 	if counter == counter_max :
 		super(b)
